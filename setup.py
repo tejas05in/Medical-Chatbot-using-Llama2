1ef0a89e343e83397bb11c5c1198fda19a6fd39a
Dr. TEJAS. J <tejas05in@yahoo.co.in>
2023-12-31 00:26:17 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Medical Chatbot",
+    version='0.0.0',
+    author="Dr. TEJAS. J",
+    author_email="tejas05in@yahoo.co.in",
+    description="A medical chatbot trained on Gale Encyclopedia of Medicine vol 1",
+    packages=find_packages(),
+    install_requires=[]
+)
